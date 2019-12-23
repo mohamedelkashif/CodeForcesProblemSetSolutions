42cee6757dace5db0e6a6a94f41bb5f4b09ece94
MohamedKashif <mohamed.kashif@cegedim.com>
2019-12-23 14:41:23 +0200

    59 - problem - A

--- Category - A/Word.java
+++ Category - A/Word.java
@@ -1,31 +1,27 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.lang.*;
 public class Main{
-     public static void main(String[] args){
-          Scanner sc=new Scanner(System.in);  
-          String word = sc.nextLine();
-          char[] chars = word.toCharArray();
-          int upeerCount = 0;
-          int lowerCount = 0;
-          for(int i=0;i<chars.length;i++){
-               if(Character.isUpperCase(chars[i]) == true){
-                    upeerCount++;
-               }
-               else if(Character.isLowerCase(chars[i]) == true){
-                    lowerCount++;
-               }
-             
-               
-               
-          }
-            if(lowerCount > upeerCount){
-                    System.out.println(word.toLowerCase());
-               }
-               else if(lowerCount < upeerCount){
-                    System.out.println(word.toUpperCase());
-               }
-               else{
-                    System.out.println(word.toLowerCase());
-               }
-          
-     }
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        String val = input.nextLine();
+        int capitalCounter = 0;
+        int smallCounter = 0;
+        for(int i =0;i<val.length();i++){
+            if(Character.isUpperCase(val.charAt(i))){
+                capitalCounter++;
+            }
+            else{
+                smallCounter++;
+            }
+        }
+        if(capitalCounter > smallCounter){
+            System.out.println(val.toUpperCase());
+        }
+        else if(smallCounter > capitalCounter){
+            System.out.println(val.toLowerCase());
+        }
+        else{
+            System.out.println(val.toLowerCase());
+        }
+    }
 }
