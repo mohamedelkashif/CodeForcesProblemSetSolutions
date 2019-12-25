497dd62b8d1f309f370ddbeb3a6c91bc3dd8c7a0
mohamedelkashif <mohamed_elkashif@windowslive.com>
2019-12-25 18:54:31 +0200

    431 - problem - A

--- Category - A/BlackSquare.java
+++ Category - A/BlackSquare.java
@@ -1,40 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 public class Main{
-     public static void main(String[] args)  {
-		// TODO Auto-generated method stub
-		 
-		Scanner sc = new Scanner(System.in);
-        int firstSum=0;
-        int secondSum = 0;
-        int thirdSum=0;
-        int fourthSum=0;
-        int num=0;
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i=0;i<4;i++){
-        	//arr[i] = sc.nextInt();
-        	num = sc.nextInt();
-            list.add(num);
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int a3 = in.nextInt();
+        int a4 = in.nextInt();
+        
+        int count = 0;
+        
+        in.nextLine();
+        String str = in.nextLine();
+        
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '1'){
+                count += a1;
+            }
+            if(str.charAt(i) == '2'){
+                count += a2;
+            }
+            if(str.charAt(i) == '3'){
+                count += a3;
+            }
+            if(str.charAt(i) == '4'){
+                count += a4;
+            }
         }
-       sc.nextLine();
-       String str = sc.nextLine();
-       char [] chars = str.toCharArray();
-       for(int i=0;i<chars.length;i++){
-    	   if(chars[i] == '1'){
-    		   firstSum+=list.get(0);
-    	   }
-    	   else if(chars[i] == '2'){
-    		   secondSum+=list.get(1);
-    	   }
-    	   else if(chars[i] == '3'){
-    		   thirdSum+=list.get(2);
-    	   }
-    	   else if(chars[i] == '4'){
-    		   fourthSum+=list.get(3);
-    	   }
-       }
-         System.out.println(firstSum+secondSum+thirdSum+fourthSum);
-	}
-
+        System.out.println(count);
+    }
 }
